24b13552b44497473707655d2abf6353d87eb8e8
Soklie <meurn.sokly.2823@rupp.edu.kh>
2026-01-08 13:50:01 +0700

    Update again

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
+'use client'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./user/countries/components/Navbar";
-import Footer from "./user/countries/components/Footer";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -10,11 +9,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Navbar/>
-        <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-gray-50">
-          {children}
-        </main>
-        <Footer/>
+        {children}
       </body>
     </html>
   );
---
+++ app/user/layout.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../user/countries/components/Navbar";
+import Footer from "../user/countries/components/Footer";
+
+export default function MainLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Navbar/>
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-gray-50">
+        {children}
+      </main>
+      <Footer/>
+    </>
+  );
+}
